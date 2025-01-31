7d00fceba6f576407178cc6df4dd1075aa9d18b6
Paulo Andre <pauloandresdsf@gmail.com>
2025-01-30 21:59:57 -0300

    Correção na function add amg

--- challenge-amigo-secreto/app.js
+++ challenge-amigo-secreto/app.js
@@ -1,1 +1,33 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Array para armazenar os nomes
+let listaAmigos = [];
+
+// Função para adicionar amigo
+function adicionarAmigo() {
+    let nomeAmigo = document.querySelector("#amigo");
+    let valorAmigo = nomeAmigo.value.trim(); // Pega o valor corretamente
+
+     // Verifica se o nome é válido
+    if (valorAmigo == '') {
+        alert('Insira nome válido');
+        nomeAmigo.value = '';
+        return;
+    }
+    
+    // Verifica se o nome contém números
+    if (/\d/.test(valorAmigo)) {
+    alert('Insira nome valido, atualmente possue numeros');
+    nomeAmigo.value = '';
+    mensagemInicial();
+}else {
+    listaAmigos.push(valorAmigo);
+    nomeAmigo.value = '';
+    mostrarUL();
+}
+ 
+function mostrarUL () {
+    let listaAmigos = document.querySelector('ul');
+    const ultimoAmigo = listaAmigos[listaAmigos.length - 1];
+    let li = document.createElement('li');
+    li.textContent = ultimoAmigo;
+    listaHTML.appendchild(li);
+}
